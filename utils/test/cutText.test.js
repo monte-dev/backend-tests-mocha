24b39a42e7b2b5894159f3b7b3f580c772981627
monte-dev <badziobw@gmail.com>
2023-07-19 23:54:22 +0100

    add initial tests

---
+++ utils/test/cutText.test.js
@@ -0,0 +1,37 @@
+const cutText = require('../cutText.js');
+const expect = require('chai').expect;
+
+describe('CutText', () => {
+	it('should return an error if "content" arg is not a string', () => {
+		if (typeof content !== 'string') return 'Error';
+		expect(cutText(undefined, 20)).to.equal('Error');
+		expect(cutText(12, 20)).to.equal('Error');
+		expect(cutText({}, 20)).to.equal('Error');
+		expect(cutText([], 20)).to.equal('Error');
+		expect(cutText(function () {}, 20)).to.equal('Error');
+	});
+
+	it('should return an error if "content" arg length is 0', () => {
+		expect(cutText('', 20)).to.equal('Error');
+	});
+
+	it('should return an error if "maxLength" arg is not a number', () => {
+		if (typeof content !== 'string') return 'Error';
+
+		expect(cutText('Lorem Ipsum', undefined)).to.equal('Error');
+		expect(cutText('Lorem Ipsum', 'abc')).to.equal('Error');
+		expect(cutText('Lorem Ipsum', {})).to.equal('Error');
+		expect(cutText('Lorem Ipsum', [])).to.equal('Error');
+		expect(cutText('Lorem Ipsum', function () {})).to.equal('Error');
+	});
+
+	it('should return an error if "maxLength" is lower or equal 0', () => {
+		expect(cutText('Lorem Ipsum', 0)).to.equal('Error');
+		expect(cutText('Lorem Ipsum', -6)).to.equal('Error');
+	});
+
+	it('should return "content" without changes if proper args', () => {
+		expect(cutText('Lorem Ipsum', 40)).to.equal('Lorem Ipsum');
+		expect(cutText('Lorem Ipsum', 11)).to.equal('Lorem Ipsum');
+	});
+});
